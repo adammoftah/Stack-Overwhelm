5c4614f882f7cf5df5bb50aee544402a60a8b7ec
Adam <adam.moftah@gmail.com>
2020-04-06 14:01:11 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+    context: __dirname,
+    entry: './frontend/stack_overwhekm.jsx', //modify this
+    output: {
+        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    query: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                },
+            }
+        ]
+    },
+    devtool: 'source-map',
+    resolve: {
+        extensions: [".js", ".jsx", "*"]
+    }
+};
