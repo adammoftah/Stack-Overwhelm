5a90c6ee2aca5788cc0513622db8ade1ebd370cf
Adam <adam.moftah@gmail.com>
2020-04-09 00:52:01 -0400

    added basics of navbar

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Provider } from 'react-redux';
 import { Link, Route, Switch } from 'react-router-dom';
 
-import GreetingContainer from './greeting/greeting_container';
+import HeaderContainer from './header/header_container';
 import SignUpFormContainer from './session_form/signup_form_container';
 import LogInFormContainer from './session_form/login_form_container';
 
@@ -10,11 +10,7 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 
 const App = () => (
     <>
-        <Header />
-        <header>
-            <h1>StackOverwhelm</h1>
-            <GreetingContainer />
-        </header>
+        <HeaderContainer />
         <Switch>
             <AuthRoute exact path="/login" component={LogInFormContainer} />
             <AuthRoute exact path="/signup" component={SignUpFormContainer} />
--- frontend/components/header/header.jsx
+++ frontend/components/header/header.jsx
@@ -1,18 +1,52 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 class Header extends React.Component {
     constructor(props) {
         super(props)
     }
 
     render() {
+        const { currentUser, logout } = this.props;
         const loggedIn = () => (
             <>
             </>
         );
+
         const loggedOut = () => (
             <>
             </>
         );
-        return currentUser ? loggedIn() : loggedOut();
+
+        const sessionLinks = () => (
+            <nav className="session-button-nav">
+                <Link className="session-button session-button-log" to="/login">Log in</Link>
+                &nbsp;
+                <Link className="session-button session-button-signup" to="/signup">Sign up</Link>
+            </nav>
+        );
+        const greeting = () => (
+            <>
+                <span>Hi, {currentUser.display_name}!</span>
+                <nav className="session-button-nav">
+                    <Link className="session-button session-button-log" to="/signup">Log out</Link>
+                </nav>
+            </>
+        );
+
+        return (
+            <header className="navbar">
+                <div className="navbar-items">
+                    <img id="menu-button" src="/images/menu.png"></img>
+                    <span>StackOverwhelm</span>
+                    {currentUser ? greeting() : sessionLinks()}
+                </div>
+
+            </header>
+        )
+        //return currentUser ? loggedIn() : loggedOut();
     }
 
 }
+
+export default Header;
