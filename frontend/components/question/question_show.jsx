2d84483ee0c45b80aac26b824ffd1ca0af9fae2b
Adam <adam.moftah@gmail.com>
2020-05-27 23:10:45 -0400

    implement total vote count in question show componenenent

--- frontend/components/question/question_show.jsx
+++ frontend/components/question/question_show.jsx
@@ -1,138 +1,157 @@
-import React from 'react';
-import AnswerContainer from '../answer/answer_container';
-import { Link } from 'react-router-dom';
+import React from "react";
+import AnswerContainer from "../answer/answer_container";
+import { Link } from "react-router-dom";
 
 class QuestionShow extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            body: ""
-        };
-        this.submitAnswer = this.submitAnswer.bind(this);
-        this.resizeElement = React.createRef();
+  constructor(props) {
+    super(props);
+    this.state = {
+      body: "",
     };
+    this.submitAnswer = this.submitAnswer.bind(this);
+    this.resizeElement = React.createRef();
+  }
 
-    componentWillMount() {
-        this.props.fetchOneQuestion(this.props.questionId);
-    }
+  componentWillMount() {
+    this.props.fetchOneQuestion(this.props.questionId);
+  }
 
-    componentDidMount() {
+  componentDidMount() {
+    this.setState({
+      resizing: false,
+      currentBodyHeight: 200,
+    });
+
+    this.resizeElement.current.addEventListener("mousedown", (e) => {
+      this.setState({
+        resizing: true,
+        resizingY: e.clientY,
+      });
+    });
+
+    document.addEventListener("mousemove", (e) => {
+      if (this.state.resizing) {
+        const newHeight =
+          this.state.currentBodyHeight - (this.state.resizingY - e.clientY);
         this.setState({
-            resizing: false,
-            currentBodyHeight: 200
+          currentBodyHeight: newHeight,
+          resizingY: e.clientY,
         });
+      }
+    });
 
-        this.resizeElement.current.addEventListener('mousedown', (e) => {
-            this.setState({
-                resizing: true,
-                resizingY: e.clientY
-            });
-        });
+    document.addEventListener("mouseup", () => {
+      this.setState({
+        resizing: false,
+      });
+    });
+  }
 
-        document.addEventListener('mousemove', (e) => {
-            if (this.state.resizing) {
-                const newHeight = this.state.currentBodyHeight - (this.state.resizingY - e.clientY);
-                this.setState({
-                    currentBodyHeight: newHeight,
-                    resizingY: e.clientY
-                });
-            }
-        });
+  update(field) {
+    return (event) =>
+      this.setState({
+        [field]: event.currentTarget.value,
+      });
+  }
 
-        document.addEventListener('mouseup', () => {
-            this.setState({
-                resizing: false
-            });
-        });
-    };
+  submitAnswer() {
+    this.props.submitAnswer(this.props.questionId, this.state.body);
+    this.state.body = "";
+  }
 
-    update(field) {
-        return event => this.setState({
-            [field]: event.currentTarget.value
-        });
-    };
-
-    submitAnswer() {
-        this.props.submitAnswer(this.props.questionId, this.state.body);
-        this.state.body = "";
-    }
-
-    render() {
-        return (
-            <>
-                <div id="question-show-page">
-                    <div id="question-show-container">
-                        <div id="question-show-title">
-                            <div className="question-show-title-row">
-                                <div id="question-show-title-header">{this.props.question.title}</div>
-                                <Link className="session-button ask-question-button" to="/questions/ask">Ask Question</Link>
-                            </div>
-                            <div className="question-show-title-row question-show-activity">
-                                <span className="question-show-activity-pair">
-                                    <span className="question-show-activity-key">Asked</span>&nbsp;
-                                    <time dateTime={this.props.question.created_at}>today</time>
-                                </span>
-
-                                <span className="question-show-activity-pair">
-                                    <span className="question-show-activity-key">Active</span>&nbsp;
-                                    <time dateTime={this.props.question.updated_at}>today</time>
-                                </span>
-                            </div>
-                        </div>
-
-                        <div id="question-show-mainbar">
-                            <div id="question-show-question">
-                                <div className="question-show-grid question-show-vote-container">
-                                    <div className="question-show-vote">
-                                        <button className="question-show-vote-grid question-show-vote-grid-vote"
-                                            onClick={() => this.props.upvote(this.props.question.id)}>
-                                            <svg width="36" height="36" viewBox="0 0 36 36">
-                                                <path d="M2 26h32L18 10 2 26z" />
-                                            </svg>
-                                        </button>
-                                        <div className="question-show-vote-grid question-show-vote-score">0</div>
-                                        <button className="question-show-vote-grid question-show-vote-grid-vote"
-                                            onClick={() => this.props.downvote(this.props.question.id)}>
-                                            <svg width="36" height="36" viewBox="0 0 36 36">
-                                                <path d="M2 10h32L18 26 2 10z" />
-                                            </svg>
-                                        </button>
-                                    </div>
-                                </div>
-                                <div className="question-show-grid question-show-post">
-                                    <div className="question-show-post-body">
-                                        {this.props.question.body}
-                                    </div>
-                                </div>
-                            </div>
-
-                            {this.props.answers.map(answer => (
-                                <AnswerContainer
-                                    key={answer.id}
-                                    answer={answer}
-                                />
-                            ))}
-                            <form onSubmit={this.submitAnswer}>
-
-                                <label className="question-form-field-container">
-                                    Your Answer
-                                    <textarea className="question-form-field-body"
-                                        onChange={this.update("body")}
-                                        value={this.state ? this.state.body : ""}
-                                        style={{ height: this.state.currentBodyHeight + 'px' }} />
-                                    <div id="question-form-field-body-grippie-container" ref={this.resizeElement}>
-                                        <div id="question-form-field-body-grippie" />
-                                    </div>
-                                </label>
-                                <input type="Submit" />
-                            </form>
-                        </div>
-
-                    </div>
+  render() {
+    return (
+      <>
+        <div id="question-show-page">
+          <div id="question-show-container">
+            <div id="question-show-title">
+              <div className="question-show-title-row">
+                <div id="question-show-title-header">
+                  {this.props.question.title}
                 </div>
-            </>
-        )
-    }
-};
+                <Link
+                  className="session-button ask-question-button"
+                  to="/questions/ask"
+                >
+                  Ask Question
+                </Link>
+              </div>
+              <div className="question-show-title-row question-show-activity">
+                <span className="question-show-activity-pair">
+                  <span className="question-show-activity-key">Asked</span>
+                  &nbsp;
+                  <time dateTime={this.props.question.created_at}>today</time>
+                </span>
+
+                <span className="question-show-activity-pair">
+                  <span className="question-show-activity-key">Active</span>
+                  &nbsp;
+                  <time dateTime={this.props.question.updated_at}>today</time>
+                </span>
+              </div>
+            </div>
+
+            <div id="question-show-mainbar">
+              <div id="question-show-question">
+                <div className="question-show-grid question-show-vote-container">
+                  <div className="question-show-vote">
+                    <button
+                      className="question-show-vote-grid question-show-vote-grid-vote"
+                      onClick={() => this.props.upvote(this.props.question.id)}
+                    >
+                      <svg width="36" height="36" viewBox="0 0 36 36">
+                        <path d="M2 26h32L18 10 2 26z" />
+                      </svg>
+                    </button>
+                    <div className="question-show-vote-grid question-show-vote-score">
+                      {this.props.votes}
+                    </div>
+                    <button
+                      className="question-show-vote-grid question-show-vote-grid-vote"
+                      onClick={() =>
+                        this.props.downvote(this.props.question.id)
+                      }
+                    >
+                      <svg width="36" height="36" viewBox="0 0 36 36">
+                        <path d="M2 10h32L18 26 2 10z" />
+                      </svg>
+                    </button>
+                  </div>
+                </div>
+                <div className="question-show-grid question-show-post">
+                  <div className="question-show-post-body">
+                    {this.props.question.body}
+                  </div>
+                </div>
+              </div>
+
+              {this.props.answers.map((answer) => (
+                <AnswerContainer key={answer.id} answer={answer} />
+              ))}
+              <form onSubmit={this.submitAnswer}>
+                <label className="question-form-field-container">
+                  Your Answer
+                  <textarea
+                    className="question-form-field-body"
+                    onChange={this.update("body")}
+                    value={this.state ? this.state.body : ""}
+                    style={{ height: this.state.currentBodyHeight + "px" }}
+                  />
+                  <div
+                    id="question-form-field-body-grippie-container"
+                    ref={this.resizeElement}
+                  >
+                    <div id="question-form-field-body-grippie" />
+                  </div>
+                </label>
+                <input type="Submit" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
 
 export default QuestionShow;
--- frontend/components/question/question_show_container.jsx
+++ frontend/components/question/question_show_container.jsx
@@ -1,39 +1,48 @@
-import { connect } from 'react-redux';
-import { fetchQuestion } from '../../actions/questions_actions';
-import { createAnswer } from '../../actions/answers_actions';
-import { upvoteQuestion, downvoteQuestion } from '../../actions/votes_actions';
-import QuestionShow from './question_show';
+import { connect } from "react-redux";
+import { fetchQuestion } from "../../actions/questions_actions";
+import { createAnswer } from "../../actions/answers_actions";
+import { upvoteQuestion, downvoteQuestion } from "../../actions/votes_actions";
+import QuestionShow from "./question_show";
 
 const mapStateToProps = (state, ownProps) => {
-    const questionId = ownProps.match.params.questionId;
-    let question = {};
-    let author = {};
-    let answers = [];
-    if (state.entities.questions && state.entities.questions[questionId]) {
-        const temp_question = state.entities.questions[questionId];
-        question = temp_question.question;
-        author = temp_question.authors[question.author_id];
-        console.log(state);
-        Object.keys(state.entities.answers).forEach(answerId => {
-            const answer = state.entities.answers[answerId];
-            if (answer) answers.push(answer);
-        });
-    }
+  const questionId = ownProps.match.params.questionId;
+  let question = {};
+  let author = {};
+  let answers = [];
+  let votes = 0;
+  let currentUserVote = 0;
+  if (state.entities.questions && state.entities.questions[questionId]) {
+    const tempQuestion = state.entities.questions[questionId];
+    question = tempQuestion.question;
+    author = tempQuestion.authors[question.author_id];
+    Object.keys(tempQuestion.votes).forEach((vote) => {
+      let currentVote = tempQuestion.votes[vote];
+      votes += currentVote.value;
+      if (currentVote.userId === state.session.id) {
+        currentUserVote = currentVote.value;
+      }
+    });
+    Object.keys(state.entities.answers).forEach((answerId) => {
+      const answer = state.entities.answers[answerId];
+      if (answer) answers.push(answer);
+    });
+  }
 
-    return {
-        questionId,
-        answers,
-        author,
-        question,
-    };
+  return {
+    questionId,
+    answers,
+    author,
+    question,
+    votes,
+    currentUserVote,
+  };
 };
 
-const mapDispatchToProps = dispatch => ({
-    fetchOneQuestion: questionId => dispatch(fetchQuestion(questionId)),
-    submitAnswer: (questionId, body) => dispatch(createAnswer(questionId, body)),
-    upvote: questionId => dispatch(upvoteQuestion(questionId)),
-    downvote: questionId => dispatch(downvoteQuestion(questionId)),
+const mapDispatchToProps = (dispatch) => ({
+  fetchOneQuestion: (questionId) => dispatch(fetchQuestion(questionId)),
+  submitAnswer: (questionId, body) => dispatch(createAnswer(questionId, body)),
+  upvote: (questionId) => dispatch(upvoteQuestion(questionId)),
+  downvote: (questionId) => dispatch(downvoteQuestion(questionId)),
 });
-
 
 export default connect(mapStateToProps, mapDispatchToProps)(QuestionShow);
